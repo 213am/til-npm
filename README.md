ff57fd75905e5b2439c1088023a150bdc3c36530
213am <dev.213am@gmail.com>
2024-12-23 12:20:16 +0900

    [dosc] react-hook-form 정리

--- README.md
+++ README.md
@@ -298,7 +298,9 @@ useEffect(() => {
 const loginSchema = yup.object({
   file: yup
     .mixed()
-    .required("첨부 파일을 넣어주세요")
+    .test("required", "첨부 파일을 업로드 해주세요", (value) => {
+      return value && value.length > 0;
+    })
     .test("filesize", "파일 크기는 2MB 이하만 가능합니다.", (value) => {
       return value && value[0]?.size <= 2 * 1024 * 1024; // 2MB 이하
     }),
@@ -318,7 +320,9 @@ const loginSchema = yup.object({
 const loginSchema = yup.object({
   profileImg: yup
     .mixed()
-    .required("프로필 이미지를 추가해주세요")
+    .test("required", "프로필 이미지를 업로드 해주세요", (value) => {
+      return value && value.length > 0;
+    })
     .test("filesize", "파일 크기는 2MB 이하만 가능합니다.", (value) => {
       return value && value[0]?.size <= 2 * 1024 * 1024; // 2MB 이하
     })
@@ -337,4 +341,255 @@ const loginSchema = yup.object({
 />
 {/* 오류 메세지 */}
 <p style={{ color: "red" }}>{errors.profileImg?.message}</p>
+```
+
+### 4.3. 여러 파일 첨부하기
+
+```jsx
+  {/* 여러 파일 첨부하기 */}
+<label>여러개의 파일 첨부하기</label>
+<input type="file" multiple {...register("ufiles")} />
+{/* 오류 메세지 */}
+<p style={{ color: "red" }}>{errors.ufiles?.message}</p>
+```
+
+```jsx
+const loginSchema = yup.object({
+  ufiles: yup
+    .mixed()
+    .test("required", "1개 이상의 파일을 업로드 해주세요", (value) => {
+      return value && value.length > 0;
+    })
+    .test("filesize", "파일 크기는 2MB 이하만 가능합니다.", (value) => {
+      return (
+        // 파일들이 있다면 && 모든 파일들을 배열로 변환해 조건이 맞는지 반복해서 비교한다.
+        // every 는 모두 true 인 경우만 true 를 return 한다
+        // 하나라도 false 가 나오면 false 를 return 한다
+        value &&
+        Array.from(value).every((file) => {
+          file.size <= 2 * 1024 * 1024;
+        })
+      );
+    })
+    .test("fileCount", "최대 3개의 파일만 업로드 가능합니다", (value) => {
+      return value && value.length <= 3;
+    }),
+});
+```
+
+### 4.4. 여러 이미지 파일 첨부하기
+
+```jsx
+ {/* 여러 이미지 첨부하기 */}
+<label>여러개의 이미지 첨부하기</label>
+<input
+  type="file"
+  accept="image/png, image/jpeg"
+  multiple
+  {...register("uimgfiles")}
+/>
+{/* 오류 메세지 */}
+<p style={{ color: "red" }}>{errors.uimgfiles?.message}</p>
+```
+
+```jsx
+const loginSchema = yup.object({
+  uimgfiles: yup
+    .mixed()
+    .test("required", "1개 이상의 이미지를 업로드 해주세요", (value) => {
+      return value && value.length > 0;
+    })
+    .test("fileCount", "최대 3개의 이미지만 업로드 가능합니다", (value) => {
+      return value && value.length <= 3;
+    })
+    .test("fileType", "JPG 또는 PNG 파일만 업로드 가능합니다.", (value) => {
+      // 파일이 1개가 아니고 여러개이므로 반복문으로 type 비교를 해야 함.
+      return (
+        value &&
+        Array.from(value).every((file) =>
+          ["image/jpeg", "image/png"].includes(file.type),
+        )
+      );
+    })
+    .test("filesize", "파일 크기는 2MB 이하만 가능합니다.", (value) => {
+      // 파일이 1개가 아니고 여러개 이므로 반복문으로 각각의 파일의 크기를 알아내야한다
+      return (
+        value &&
+        Array.from(value).every((file) => {
+          file.size <= 2 * 1024 * 1024;
+        })
+      );
+    }),
+});
+```
+
+## 5. 파일 미리보기
+
+### 5.1. 1개의 이미지 미리보기
+
+```jsx
+{/* 1개 이미지 미리보기 */}
+<label>이미지 1개 미리보기</label>
+<input
+  type="file"
+  accept="image/png, image/jpeg"
+  {...register("previewfile")}
+  onChange={changePreviewHandler}
+/>
+{/* 오류 메세지 */}
+<p style={{ color: "red" }}>{errors.previewfile?.message}</p>
+{preview && (
+<div>
+  <h3>이미지 미리보기</h3>
+  <img
+    src={preview}
+    alt="첨부파일 미리보기"
+    style={{ width: 200, height: 200 }}
+  />
+</div>
+)}
+```
+
+```jsx
+const loginSchema = yup.object({
+  previewfile: yup
+    .mixed()
+    .test("required", "미리보기 할 이미지를 업로드 해주세요", (value) => {
+      return value && value.length > 0;
+    })
+    .test("fileType", "JPG 또는 PNG 파일만 업로드 가능합니다.", (value) => {
+      return value && ["image/jpeg", "image/png"].includes(value[0]?.type);
+    })
+    .test("filesize", "파일 크기는 2MB 이하만 가능합니다.", (value) => {
+      return value && value[0]?.size <= 2 * 1024 * 1024; // 2MB 이하
+    }),
+});
+
+// 이미지 미리보기 state
+const [preview, setPreview] = useState("");
+
+// 이미지 한장 미리보기
+const changePreviewHandler = (e) => {
+  const file = e.target.files[0];
+  if (file) {
+    // 웹브라우저에 임시 이미지 URL 을 생성해야 함
+    // 선택된 파일을 웹브라우저 cache 에 저장되어있음
+    // 이를 이용해 임시 url 을 생성함
+    // blob 을 생생해줌
+    // Blob(Binary Large Object) - 이미지, 사운드, 비디오와 같은 멀티미디어 데이터를 다룰 때 사용
+    setPreview(URL.createObjectURL(file));
+  } else {
+    setPreview(null);
+  }
+};
+```
+
+### 5.2. 여러개의 이미지 미리보기
+
+```jsx
+{/* 여러개 이미지 미리보기 */}
+<label>이미지 여러장 미리보기</label>
+<input
+  type="file"
+  multiple
+  accept="image/png, image/jpeg"
+  {...register("previewlist")}
+  onChange={(e) => changePreviewListHandler(e)}
+/>
+{/* 오류 메세지 */}
+<p style={{ color: "red" }}>{errors.previewlist?.message}</p>
+<div>
+  <h3>여러개의 이미지 미리보기</h3>
+  {previewList.map((item, index) => {
+    return (
+      <img
+        key={index}
+        src={item}
+        alt="여러개의 첨부파일 미리보기"
+        style={{ width: 200, height: 200 }}
+      />
+    );
+  })}
+</div>
+```
+
+```jsx
+const [previewList, setPreviewList] = useState([]);
+
+// 이미지 여러장 미리보기
+const changePreviewListHandler = (e) => {
+  const files = Array.from(e.target.files);
+
+  const list = files.map((item) => {
+    URL.createObjectURL(item);
+  });
+  setPreviewList([...list]);
+};
+
+const loginSchema = yup.object({
+  previewlist: yup
+    .mixed()
+    .test("required", "미리보기 할 이미지를 업로드 해주세요", (value) => {
+      return value && value.length > 0;
+    })
+    .test("fileCount", "최대 3개의 이미지만 업로드 가능합니다", (value) => {
+      return value && value.length <= 3;
+    })
+    .test("fileType", "JPG 또는 PNG 파일만 업로드 가능합니다.", (value) => {
+      // 파일이 1개가 아니고 여러개이므로 반복문으로 type 비교를 해야 함.
+      return (
+        value &&
+        Array.from(value).every((file) => {
+          return ["image/jpeg", "image/png"].includes(file.type);
+        })
+      );
+    })
+    .test("filesize", "파일 크기는 2MB 이하만 가능합니다.", (value) => {
+      // 파일이 1개가 아니고 여러개 이므로 반복문으로 각각의 파일의 크기를 알아내야한다
+      return (
+        value &&
+        Array.from(value).every((file) => {
+          return file.size <= 2 * 1024 * 1024;
+        })
+      );
+    }),
+});
+```
+
+## 6. axios 로 파일 전송하기 주의사항
+
+### 6.1. axios 로 파일을 전송할 때 아래 구문을 준수
+
+```jsx
+const formSubmitHandler = async (data) => {
+  console.log(data);
+
+  try {
+    // string 이었다면 아래로 가능
+    // const res = await axios.post("주소", data);
+
+    // file 을 전송하려면 option 이 추가되어야 함
+    // DB 연동을 위해서는 객체 형식으로 변환이 필요
+    const sendData = new FormData();
+    sendData.append("uid", data.name);
+    sendData.append("umail", data.email);
+    sendData.append("upw", data.password);
+    sendData.append("upwconfirm", data.passwordConfirm);
+    sendData.append("ufile", data.file);
+    sendData.append("userimg", data.profileImg);
+    sendData.append("ufiles", data.ufiles);
+    sendData.append("uimgfiles", data.uimgfiles);
+    sendData.append("previewfile", data.previewfile);
+    sendData.append("previewlist", data.previewList);
+
+    const res = await axios.post("주소", data, {
+      headers: {
+        "Content-Type": "multipart/form-data", // 파일 전송 형식
+      },
+    });
+    console.log(res.data);
+  } catch (error) {
+    console.log(error);
+  }
+};
 ```
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { yupResolver } from "@hookform/resolvers/yup";
-import { useEffect } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import * as yup from "yup";
 
@@ -24,22 +25,112 @@ const loginSchema = yup.object({
     .required("비밀번호 확인이 필요합니다"),
   file: yup
     .mixed()
+    .test("required", "첨부 파일을 업로드 해주세요", (value) => {
+      return value && value.length > 0;
+    })
     .test("filesize", "파일 크기는 2MB 이하만 가능합니다.", (value) => {
       return value && value[0]?.size <= 2 * 1024 * 1024; // 2MB 이하
-    })
-    .required("첨부 파일을 넣어주세요"),
+    }),
   profileImg: yup
     .mixed()
-    .test("filesize", "파일 크기는 2MB 이하만 가능합니다.", (value) => {
-      return value && value[0]?.size <= 2 * 1024 * 1024; // 2MB 이하
+    .test("required", "프로필 이미지를 업로드 해주세요", (value) => {
+      return value && value.length > 0;
     })
     .test("fileType", "JPG 또는 PNG 파일만 업로드 가능합니다.", (value) => {
       return value && ["image/jpeg", "image/png"].includes(value[0]?.type);
     })
-    .required("프로필 이미지를 추가해주세요"),
+    .test("filesize", "파일 크기는 2MB 이하만 가능합니다.", (value) => {
+      return value && value[0]?.size <= 2 * 1024 * 1024; // 2MB 이하
+    }),
+  ufiles: yup
+    .mixed()
+    .test("required", "1개 이상의 파일을 업로드 해주세요", (value) => {
+      return value && value.length > 0;
+    })
+    .test("filesize", "파일 크기는 2MB 이하만 가능합니다.", (value) => {
+      return (
+        // 파일들이 있다면 && 모든 파일들을 배열로 변환해 조건이 맞는지 반복해서 비교한다.
+        // every 는 모두 true 인 경우만 true 를 return 한다
+        // 하나라도 false 가 나오면 false 를 return 한다
+        value &&
+        Array.from(value).every((file) => {
+          return file.size <= 2 * 1024 * 1024;
+        })
+      );
+    })
+    .test("fileCount", "최대 3개의 파일만 업로드 가능합니다", (value) => {
+      return value && value.length <= 3;
+    }),
+  uimgfiles: yup
+    .mixed()
+    .test("required", "1개 이상의 이미지를 업로드 해주세요", (value) => {
+      return value && value.length > 0;
+    })
+    .test("fileCount", "최대 3개의 이미지만 업로드 가능합니다", (value) => {
+      return value && value.length <= 3;
+    })
+    .test("fileType", "JPG 또는 PNG 파일만 업로드 가능합니다.", (value) => {
+      // 파일이 1개가 아니고 여러개이므로 반복문으로 type 비교를 해야 함.
+      return (
+        value &&
+        Array.from(value).every((file) => {
+          return ["image/jpeg", "image/png"].includes(file.type);
+        })
+      );
+    })
+    .test("filesize", "파일 크기는 2MB 이하만 가능합니다.", (value) => {
+      // 파일이 1개가 아니고 여러개 이므로 반복문으로 각각의 파일의 크기를 알아내야한다
+      return (
+        value &&
+        Array.from(value).every((file) => {
+          return file.size <= 2 * 1024 * 1024;
+        })
+      );
+    }),
+  previewfile: yup
+    .mixed()
+    .test("required", "미리보기 할 이미지를 업로드 해주세요", (value) => {
+      return value && value.length > 0;
+    })
+    .test("fileType", "JPG 또는 PNG 파일만 업로드 가능합니다.", (value) => {
+      return value && ["image/jpeg", "image/png"].includes(value[0]?.type);
+    })
+    .test("filesize", "파일 크기는 2MB 이하만 가능합니다.", (value) => {
+      return value && value[0]?.size <= 2 * 1024 * 1024; // 2MB 이하
+    }),
+  previewlist: yup
+    .mixed()
+    .test("required", "미리보기 할 이미지를 업로드 해주세요", (value) => {
+      return value && value.length > 0;
+    })
+    .test("fileCount", "최대 3개의 이미지만 업로드 가능합니다", (value) => {
+      return value && value.length <= 3;
+    })
+    .test("fileType", "JPG 또는 PNG 파일만 업로드 가능합니다.", (value) => {
+      // 파일이 1개가 아니고 여러개이므로 반복문으로 type 비교를 해야 함.
+      return (
+        value &&
+        Array.from(value).every((file) => {
+          return ["image/jpeg", "image/png"].includes(file.type);
+        })
+      );
+    })
+    .test("filesize", "파일 크기는 2MB 이하만 가능합니다.", (value) => {
+      // 파일이 1개가 아니고 여러개 이므로 반복문으로 각각의 파일의 크기를 알아내야한다
+      return (
+        value &&
+        Array.from(value).every((file) => {
+          return file.size <= 2 * 1024 * 1024;
+        })
+      );
+    }),
 });
 
 function App() {
+  // 이미지 미리보기 state
+  const [preview, setPreview] = useState("");
+  const [previewList, setPreviewList] = useState([]);
+
   const {
     register,
     handleSubmit,
@@ -61,8 +152,56 @@ function App() {
     mode: "onChange",
   });
 
-  const formSubmitHandler = (data) => {
+  // 이미지 한장 미리보기
+  const changePreviewHandler = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setPreview(URL.createObjectURL(file));
+    } else {
+      setPreview(null);
+    }
+  };
+
+  // 이미지 여러장 미리보기
+  const changePreviewListHandler = (e) => {
+    const files = Array.from(e.target.files);
+
+    const list = files.map((item) => {
+      return URL.createObjectURL(item);
+    });
+    setPreviewList([...list]);
+  };
+
+  const formSubmitHandler = async (data) => {
     console.log(data);
+
+    try {
+      // string 이었다면 아래로 가능
+      // const res = await axios.post("주소", data);
+
+      // file 을 전송하려면 option 이 추가되어야 함
+      // DB 연동을 위해서는 객체 형식으로 변환이 필요
+      const sendData = new FormData();
+      sendData.append("uid", data.name);
+      sendData.append("umail", data.email);
+      sendData.append("upw", data.password);
+      sendData.append("upwconfirm", data.passwordConfirm);
+      sendData.append("ufile", data.file);
+      sendData.append("userimg", data.profileImg);
+      sendData.append("ufiles", data.ufiles);
+      sendData.append("uimgfiles", data.uimgfiles);
+      sendData.append("previewfile", data.previewfile);
+      sendData.append("previewlist", data.previewList);
+
+      const res = await axios.post("주소", data, {
+        headers: {
+          "Content-Type": "multipart/form-data", // 파일 전송 형식
+        },
+      });
+      console.log(res.data);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   // 원하는 시점에 특정 input 에만 지정된 값을 넣기
@@ -113,6 +252,69 @@ function App() {
         />
         {/* 오류 메세지 */}
         <p style={{ color: "red" }}>{errors.profileImg?.message}</p>
+        <br />
+        {/* 여러 파일 첨부하기 */}
+        <label>여러개의 파일 첨부하기</label>
+        <input type="file" multiple {...register("ufiles")} />
+        {/* 오류 메세지 */}
+        <p style={{ color: "red" }}>{errors.ufiles?.message}</p>
+        <br />
+        {/* 여러 이미지 첨부하기 */}
+        <label>여러개의 이미지 첨부하기</label>
+        <input
+          type="file"
+          accept="image/png, image/jpeg"
+          multiple
+          {...register("uimgfiles")}
+        />
+        {/* 오류 메세지 */}
+        <p style={{ color: "red" }}>{errors.uimgfiles?.message}</p>
+        <br />
+        {/* 1개 이미지 미리보기 */}
+        <label>이미지 1개 미리보기</label>
+        <input
+          type="file"
+          accept="image/png, image/jpeg"
+          {...register("previewfile")}
+          onChange={changePreviewHandler}
+        />
+        {/* 오류 메세지 */}
+        <p style={{ color: "red" }}>{errors.previewfile?.message}</p>
+        {preview && (
+          <div>
+            <h3>이미지 미리보기</h3>
+            <img
+              src={preview}
+              alt="첨부파일 미리보기"
+              style={{ width: 200, height: 200 }}
+            />
+          </div>
+        )}
+        <br />
+        {/* 여러개 이미지 미리보기 */}
+        <label>이미지 여러장 미리보기</label>
+        <input
+          type="file"
+          multiple
+          accept="image/png, image/jpeg"
+          {...register("previewlist")}
+          onChange={(e) => changePreviewListHandler(e)}
+        />
+        {/* 오류 메세지 */}
+        <p style={{ color: "red" }}>{errors.previewlist?.message}</p>
+        <div>
+          <h3>여러개의 이미지 미리보기</h3>
+          {previewList.map((item, index) => {
+            return (
+              <img
+                key={index}
+                src={item}
+                alt="여러개의 첨부파일 미리보기"
+                style={{ width: 200, height: 200 }}
+              />
+            );
+          })}
+        </div>
         <br />
         <button type="submit">로그인하기</button>
       </form>
